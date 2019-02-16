033dbee91252f2711179e1ab5c7584048c2297b8
zhaoxin <1026243373@qq.com>
2019-02-16 18:03:47 +0800

    first commit

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Description: 描述 
+ * @author zx  
+ * @date 2019年2月16日
+ */
+@SpringBootApplication
+public class DemoApplication {
+    /**
+     * Description: 描述 
+     * @param args
+     */
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
---
+++ demo/src/test/java/com/example/demo/DemoApplicationTest.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+/**
+ * Description: 描述 
+ * @author zx  
+ * @date 2019年2月16日
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApplicationTest {
+    /**
+     * Description: 描述
+     */
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
